4d162cdaa38dbdefb03fbd236295444f0df83b74
arlw87 <arlw87@googlemail.com>
2022-01-11 17:55:25 +0000

    Create method that identifies first non repeated character in a string passed to it

--- FirstNoneRepeat.java
+++ FirstNoneRepeat.java
@@ -1,3 +1,31 @@
 public class FirstNoneRepeat {
+
+    public static void main(String ...args){
+        System.out.println("This program will identify the first none repeated character for a user entered String");
+        repeatCharacterCheck("aabbcch");
+    }
+
+    static void repeatCharacterCheck(String s){
+        
+        Character firstNoneRepeatCharacter = null;
+
+        for (int i=0; i < s.length() - 1; i++){
+            char first = s.charAt(i);
+            char second = s.charAt(i+1);
+            if (first != second){
+                if (i == s.length() - 2 || second != s.charAt(i+2)){
+                    firstNoneRepeatCharacter = second;
+                    break;
+                }
+            }
+        }
+
+        if (firstNoneRepeatCharacter == null){
+            System.out.print("All characters repeat");
+        } else {
+            System.out.println("The first none repeating character is " + firstNoneRepeatCharacter);
+        }
+
+    }
     
 }
